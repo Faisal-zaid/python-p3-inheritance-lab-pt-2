bdec7e39c9fe369f3ff3b925a15a1ddceba65cad
Faisal Zaid <zaidalideveloper@gmail.com>
2025-11-24 06:26:14 +0300

    final

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,28 @@
-class Student:
-    pass
+#!/usr/bin/env python
 
+# Superclass
+class Student:
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
+
+
+# Subclass
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        # Call the parent hello() method
+        super().hello()
+        # Add the chatty extra message
+        print(
+            "How are you doing today? I'm okay, but I'm kind of tired. "
+            "Did you watch The Walking Dead last night? You didn't?! "
+            "Oh man, it was so crazy! What, you don't want any spoilers? "
+            "Okay well let me just tell you who died..."
+        )
+
+    def raise_hand(self):
+        # Call the parent raise_hand() method 10 times
+        for _ in range(10):
+            super().raise_hand()
